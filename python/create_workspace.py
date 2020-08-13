ff60d9b5fb6b782b33925c58685c2a06219e3cdb
Marc LeBlanc <mleblanc@localhost.localdomain>
2020-08-13 12:30:26 -0400

    tf version, working dir and repo options

--- python/create_workspace.py
+++ python/create_workspace.py
@@ -19,16 +19,21 @@ def createWorkspace(args):
     create_workspace_url = f'https://{TFE_ADDRESS}/api/v2/organizations/{TFE_ORGANIZATION}/workspaces'
 
     headers = {'Authorization': 'Bearer ' + TFE_API_TOKEN, 'Content-Type': 'application/vnd.api+json'}
-    payload = {'data': { 'attributes': { 'name': args.workspace_name }, 'type': 'workspaces' }}
 
+    if args.repo is not 0:
+        payload = { 'data': { 'attributes': { 'name': args.workspace_name, 'terraform_version': args.tf_version, 'working-directory': args.working_dir, 'vcs-repo': { 'identifier': args.repo, 'oauth-token-id': args.oauth_token, 'branch': '' , 'default-branch': True } }, 'type': 'workspaces' }}
+    else:
+        payload = {'data': { 'attributes': { 'name': args.workspace_name }, 'type': 'workspaces' }}
+    #print(payload) usefulfor debug
     r = requests.post(create_workspace_url, data = json.dumps(payload), headers = headers)
 
     if r.status_code == 201:
-        print(f':white_check_mark: Workspace [bold]{args.workspace_name}[/bold] creation completed...')
+        print(f'[green]:white_check_mark:[/green] Workspace [bold]{args.workspace_name}[/bold] creation completed...')
     else:
+        print (r.json())
         error_response = f'{r.json()["errors"][0]["title"]}: {r.json()["errors"][0]["detail"]}'
 
-        print(f':x: Workspace [bold]{args.workspace_name}[/bold] creation failed.. got: {error_response}')
+        print(f'[red]:x:[/red] Workspace [bold]{args.workspace_name}[/bold] creation failed.. got: {error_response}')
 
 def main():
     parser = argparse.ArgumentParser(description='Process workspace arguments.')
@@ -36,7 +41,20 @@ def main():
     parser.add_argument('--workspace', '-w', dest='workspace_name', required=True,
                         help='sets the name of the workspace to create')
 
+    parser.add_argument('--tf-version', '-t', dest='tf_version',
+                        help='(optional) specify version of Terraform for the workspace')
+
+    parser.add_argument('--repo', '-r', dest='repo', default=0,
+                        help='(optional) specify version of Terraform for the workspace')
+
+    parser.add_argument('--wd', '-d', dest='working_dir',
+                        help='(optional) specify version of Terraform for the workspace')
+
+    parser.add_argument('--ot', '-o', dest='oauth_token',
+                        help='(optional) specify version of Terraform for the workspace')
+
     args = parser.parse_args()
+    print(args)
 
     createWorkspace(args)
 
